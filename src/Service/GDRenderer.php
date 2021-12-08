f4877d9fccbf160c6eafdbe6bb9db57f1395d909
Gordon Anderson <gordon.b.anderson@gmail.com>
2021-12-08 22:48:05 +0000

    FIX: Remove warning message that is breaking some tests

--- src/Service/GDRenderer.php
+++ src/Service/GDRenderer.php
@@ -42,7 +42,7 @@ class GDRenderer
 
             switch ($key) {
                 case 'pointradius':
-                    continue;
+                    break;
                 case 'backgroundcolor':
                 case 'strokecolor':
                 case 'fillcolor':
