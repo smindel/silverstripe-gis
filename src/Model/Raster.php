16ebbf9b07436ea88e180ce5866b369b608dc6c0
Gordon Anderson <gordon.b.anderson@gmail.com>
2021-12-11 18:39:04 +0000

    WIP: Added a var dump of the output, as CI is different from local

--- src/Model/Raster.php
+++ src/Model/Raster.php
@@ -47,6 +47,8 @@ class Raster
             $output = `$cmd`;
             error_log('T3:' . print_r($output, true));
 
+            var_dump($output);
+
             if (preg_match('/\WAUTHORITY\["EPSG","([^"]+)"\]\]$/', $output, $matches)) {
                 error_log('T4: MATCHES: ' . print_r($matches, true));
                 $this->info['srid'] = $matches[1];
